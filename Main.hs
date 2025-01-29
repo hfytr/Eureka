ba220f427b22811cd227de6dc2c2c9e34e3bd47d
Archim J <archimj@proton.me>
2025-01-29 16:55:22 -0500

    changes

--- Main.hs
+++ Main.hs
@@ -1,9 +1,19 @@
 module Main where
 
-import Board qualified as B
+import Board
+import Control.Monad.ST (runST)
+import Data.Massiv.Array
 import Data.Maybe (fromJust)
+import Debug.Trace
+
+startFen :: String
+startFen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 main :: IO ()
-main = do
-  let board = fromJust $ B.parseFen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-  print $ B.genMoves board
+main =
+  print $
+    runST $ do
+      board <- fromJust <$> parseFen "rnbqkbnr/ppp1p1pp/B7/3p1p2/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 1 3"
+      (moves, movesLen) <- genMoves board
+      makeMove board $ newMove 4 6 Rook Castle
+      unmakeMove board
--- src/Board.hs
+++ src/Board.hs
@@ -3,55 +3,67 @@
 
 module Board where
 
-import Control.Monad (foldM)
+import Control.Monad (foldM, forM_, when)
 import Control.Monad.ST (ST, runST)
 import Data.Bits (bit, complement, countTrailingZeros, popCount, shiftL, shiftR, testBit, xor, (.&.), (.^.), (.|.))
-import Data.Char (ord)
+import Data.Char (chr, ord)
 import Data.List (unfoldr)
+import Data.List.Split (chunksOf)
 import Data.Map qualified as Map
 import Data.Massiv.Array (Array, (!))
 import Data.Massiv.Array qualified as A
 import Data.Massiv.Array.Mutable (MArray)
 import Data.Massiv.Array.Mutable qualified as MA
+import Data.Maybe (fromJust)
+import Data.STRef (STRef, newSTRef, readSTRef, writeSTRef)
 import Data.Word (Word16, Word64, Word8)
+import Debug.Trace
 import GHC.Exts (Word (W#), inline, pdep#)
+import Numeric (showBin)
 import System.Random (mkStdGen, uniform)
 import Text.Read (readMaybe)
 
 type Bitboard = Word64
 type Move = Word16
 type Square = Word8
-data MoveType = Normal | Castle | EnPassant | Promotion deriving (Enum, Show)
-data Piece = Rook | Bishop | Pawn | Knight | King | Queen | Empty deriving (Enum, Show)
+data MoveType = Normal | Castle | EnPassant | Promotion deriving (Enum, Show, Eq)
+data Piece = Rook | Bishop | Pawn | Knight | King | Queen | Empty deriving (Enum, Show, Eq, Bounded)
 data Player = White | Black deriving (Enum, Show)
-data Board = Board
-  { bitbs :: Array A.P Int Bitboard
-  , sqs :: Array A.P Int Square
-  , stm :: Player
-  , castle :: ((Bool, Bool), (Bool, Bool))
-  , fiftyCount :: Word16
-  , ep :: Maybe Word8
+data Board s = Board
+  { bitbs :: MArray s A.P Int Bitboard
+  , sqs :: MArray s A.P Int Square
+  , castle :: MArray s A.P Int Int
+  , history :: MArray s A.P Int Move
+  , captured :: MArray s A.P Int Square
+  , fiftyCounts :: MArray s A.P Int Int
+  , other :: STRef s (Player, Maybe Int, Int)
   }
-  deriving (Show)
 
-genMoves :: Board -> (Array A.P Int Move, Int)
-genMoves Board{bitbs, stm, sqs, ep} = runST $
-  do
-    moves <- MA.thawS $ A.replicate A.Seq 256 0
-    len <- foldBitboardM (addPieceMoves moves) (bitbs ! fromEnum stm) 0
-    (,len) <$> MA.freezeS moves
+genMoves :: Board s -> ST s (Array A.P Int Move, Int)
+genMoves Board{bitbs, sqs, other} = do
+  (stm, _, _) <- readSTRef other
+  moves <- MA.thawS $ A.replicate A.Seq 256 0
+  myPieces <- MA.readM bitbs $ fromEnum stm
+  len <- foldBitboardM (addPieceMoves moves) myPieces 0
+  moves' <- MA.freezeS moves
+  return (moves', len)
   where
-    addPieceMoves :: MArray s A.P Int Move -> Int -> Int -> ST s Int
     addPieceMoves moves sq ind = do
-      let (piece, _) = squareInfo $ sqs ! sq
-          canMoveTo =
-            complement (bitbs ! fromEnum stm)
-              .&. case piece of
-                Pawn -> (moveLookups ! (fromEnum piece, sq)) .&. (bitbs ! (1 - fromEnum stm) .|. maybe 0 (bit . fromIntegral) ep)
-                Rook -> getSlidingAttack 0 sq
-                Bishop -> getSlidingAttack 1 sq
-                Queen -> getSlidingAttack 0 sq .|. getSlidingAttack 1 sq
-                _ -> moveLookups ! (fromEnum piece, sq)
+      (stm, ep, _) <- readSTRef other
+      (piece, _) <- squareInfo <$> MA.readM sqs sq
+      myPieces <- MA.readM bitbs $ fromEnum stm
+      oppPieces <- MA.readM bitbs $ fromEnum $ flipColor stm
+      canMoveTo <-
+        (complement myPieces .&.)
+          <$> case piece of
+            Pawn -> return $ (moveLookups ! (fromEnum piece, sq)) .&. (oppPieces .|. maybe 0 (bit . fromIntegral) ep)
+            Rook -> getSlidingAttack 0 sq
+            Bishop -> getSlidingAttack 1 sq
+            Queen -> do
+              rookAttacks <- getSlidingAttack 0 sq
+              bishopAttacks <- getSlidingAttack 1 sq
+              return $ rookAttacks .|. bishopAttacks
+            _ -> return $ moveLookups ! (fromEnum piece, sq)
       foldBitboardM
         ( \lsb l -> do
             MA.write_ moves l $ newMove sq lsb Rook Normal
@@ -60,51 +72,148 @@ genMoves Board{bitbs, stm, sqs, ep} = runST $
         canMoveTo
         ind
 
-    getSlidingAttack p sq =
-      let blockers = inline $ (moveLookups ! (p, sq)) .&. ((bitbs ! fromEnum White) .|. (bitbs ! fromEnum Black))
+    getSlidingAttack p sq = do
+      whitePieces <- inline $ MA.readM bitbs $ fromEnum White
+      blackPieces <- inline $ MA.readM bitbs $ fromEnum Black
+      let blockers = inline $ (moveLookups ! (p, sq)) .&. (whitePieces .|. blackPieces)
           ind = inline $ shiftR (blockers * fromIntegral (magicNums ! (p, sq))) (magicShift ! (p, sq))
-       in magicLookup ! (p, sq, fromIntegral ind)
+      return $ magicLookup ! (p, sq, fromIntegral ind)
 
-parseFen :: String -> Maybe Board
-parseFen fen = splitFen (words fen) >>= parseInfo
+makeMove :: Board s -> Move -> ST s Bool
+makeMove Board{bitbs, fiftyCounts, sqs, captured, castle, other, history} m = do
+  (stm, ep, gameLen) <- readSTRef other
+  let (sq1, sq2, moveT, prom) = moveInfo m
+  (toPiece, _) <- squareInfo <$> MA.readM sqs sq2
+  (fromPiece, fromColor) <- squareInfo <$> MA.readM sqs sq1
+  fiftyCount <- MA.readM fiftyCounts gameLen
+
+  traceShow fromPiece $ MA.modifyM_ bitbs (return . xor (bit sq1)) $ pieceInd fromPiece
+  MA.writeM sqs sq1 $ square White Empty
+
+  let endPiece = traceShowId $ if moveT == Promotion then prom else fromPiece
+  MA.modifyM_ bitbs (return . xor (bit sq2)) $ pieceInd endPiece
+  MA.modifyM_ bitbs (return . xor (bit sq1 .|. bit sq2)) $ fromEnum stm
+  MA.writeM sqs sq2 $ square fromColor endPiece
+
+  if traceShowId moveT == EnPassant
+    then do
+      let epSq = fromJust ep
+          captureSq = inline $ fromIntegral epSq - colorDir stm * 8
+      MA.writeM sqs captureSq $ square White Empty
+      MA.modifyM_ bitbs (return . xor (bit captureSq)) $ pieceInd Pawn
+    else when (toPiece /= Empty) $ MA.modifyM_ bitbs (return . xor (bit sq2)) $ pieceInd toPiece
+
+  let newEp = fromBool (fromPiece == Pawn && abs (sq1 - sq2) == 16) $ div (sq1 + sq2) 2
+      modifyRookCastle inStm sq =
+        when
+          (row sq `mod` 7 == 0 && col sq `mod` 7 == 0)
+          $ MA.modifyM_ castle (return . min gameLen) (fromEnum inStm * 2 + col sq `div` 2)
+
+  when (trace (showBin m "") moveT == Castle) $ do
+    let endRookSq = inline $ (sq1 + sq2) `div` 2
+        startRookSq = inline $ 56 * fromEnum (row sq1 == 7) + 7 * fromEnum (sq1 < sq2)
+        moveRookBB = return . xor (bit endRookSq .|. bit startRookSq)
+    MA.modifyM_ bitbs moveRookBB $ pieceInd Rook
+    MA.modifyM_ bitbs moveRookBB $ fromEnum stm
+    MA.write_ sqs startRookSq $ square White Empty
+    MA.write_ sqs endRookSq $ square stm Rook
+
+  when (fromPiece == King) $ do
+    MA.modifyM_ castle (return . min gameLen) $ fromEnum stm * 2
+    MA.modifyM_ castle (return . min gameLen) $ fromEnum stm * 2 + 1
+  modifyRookCastle stm sq1
+  modifyRookCastle (flipColor stm) sq2
+
+  MA.write_ captured gameLen $ square (flipColor stm) toPiece
+  MA.write_ history gameLen m
+  MA.write_ fiftyCounts (gameLen + 1) $
+    if fromPiece == Pawn || toPiece /= Empty || moveT == Castle
+      then fiftyCount + 1
+      else 0
+
+  writeSTRef other (flipColor stm, newEp, gameLen + 1)
+
+  s <- showMArr showBitboard bitbs
+  s2 <- showSqs sqs
+  return $ trace (s ++ s2) True
+
+unmakeMove :: Board s -> ST s ()
+unmakeMove Board{bitbs, sqs, captured, castle, other, history} = do
+  (stmPrev, ep, gameLen) <- readSTRef other
+  let stm = flipColor stmPrev
+  (sq1, sq2, moveT, prom) <- moveInfo <$> MA.readM history gameLen
+  (endPiece, endCol) <- squareInfo <$> MA.readM sqs sq2
+  capturedSquare <- MA.readM captured gameLen
+  let startPiece = if moveT == Promotion then prom else endPiece
+
+  MA.modifyM_ bitbs (return . xor (bit sq1)) $ pieceInd startPiece
+  MA.modifyM_ bitbs (return . xor (bit sq2)) $ pieceInd endPiece
+  MA.writeM sqs sq2 capturedSquare
+  MA.writeM sqs sq1 $ square stm startPiece
+
+  when (moveT == Castle) $ do
+    let endRookSq = inline $ (sq1 + sq2) `div` 2
+        startRookSq = inline $ 56 * fromEnum (row sq1 == 7) + 7 * fromEnum (sq1 < sq2)
+        moveRookBB = return . xor (bit endRookSq .|. bit startRookSq)
+    MA.modifyM_ bitbs moveRookBB $ pieceInd Rook
+    MA.modifyM_ bitbs moveRookBB $ fromEnum stm
+    MA.write_ sqs startRookSq $ square stm Rook
+    MA.write_ sqs endRookSq $ square White Empty
+
+  MA.forPrimM castle (\turn -> if turn == gameLen then return maxBound else return turn)
+
+  writeSTRef other (stm, Nothing, gameLen - 1)
+
+  s <- showMArr showBitboard bitbs
+  s2 <- showSqs sqs
+  return $ trace (s ++ s2) ()
+
+parseFen :: String -> ST s (Maybe (Board s))
+parseFen fen = maybe (return Nothing) parseInfo $ splitFen $ words fen
   where
-    parseInfo :: (String, String, String, String, String, String) -> Maybe Board
-    parseInfo (placement, [stmRaw], castleRaw, epRaw, fiftyCount, _) = do
-      (bitbs, sqs) <- runST $ parsePlacement placement
+    parseInfo :: (String, String, String, String, String, String) -> ST s (Maybe (Board s))
+    parseInfo (placementStr, [stmRaw], castleRaw, epRaw, fiftyCountRaw, _) =
+      do
+        history <- MA.newMArray 4096 0
+        captured <- MA.newMArray 4096 $ square White Empty
+        placement <- parsePlacement placementStr
+        fiftyCounts <- MA.newMArray 4096 0
+        MA.writeM fiftyCounts (read fiftyCountRaw) 0
+        castle <- MA.thawS $ A.fromList A.Seq [-1, -1, -1, -1]
+        forM_ castleRaw (\c -> MA.writeM castle (castleInd c) maxBound)
 
-      stm <- case stmRaw of
-        'w' -> Just White
-        'b' -> Just Black
-        _ -> Nothing
+        let res = do
+              (bitbs, sqs) <- placement
 
-      ep <- case epRaw of
-        "-" -> Just Nothing
-        (ep1 : ep2 : _) -> do
-          epCol <- parseEpCol ep1
-          epRow <- readMaybe [ep2]
-          Just $ Just $ fromIntegral $ posFromCoord (epCol, epRow)
-        _ -> Nothing
+              stm <- case stmRaw of
+                'w' -> Just White
+                'b' -> Just Black
+                _ -> Nothing
 
-      castle <-
-        foldr
-          (\c acc -> acc >>= adjustCastle c)
-          (Just ((False, False), (False, False)))
-          castleRaw
+              ep <- case epRaw of
+                "-" -> Just Nothing
+                (ep1 : ep2 : _) -> do
+                  epCol <- parseEpCol ep1
+                  epRow <- readMaybe [ep2]
+                  Just $ Just $ fromIntegral $ posFromCoord (epCol, epRow)
+                _ -> Nothing
 
-      Just $ Board bitbs sqs stm castle (read fiftyCount) ep
-    parseInfo _ = Nothing
+              Just (bitbs, sqs, stm, ep)
+        maybe
+          (return Nothing)
+          (\(bitbs, sqs, stm, ep) -> Just . Board bitbs sqs castle history captured fiftyCounts <$> newSTRef (stm, ep, 0))
+          res
+    parseInfo _ = return Nothing
 
-    parsePlacement :: String -> ST s (Maybe (Array A.P Int Bitboard, Array A.P Int Square))
+    parsePlacement :: String -> ST s (Maybe (MArray s A.P Int Bitboard, MArray s A.P Int Square))
     parsePlacement s = do
-      bitbs <- MA.thawS $ A.replicate A.Seq 8 0
-      sqs <- MA.thawS $ A.replicate A.Seq 64 $ square White Empty
+      bitbs <- MA.newMArray 8 0
+      sqs <- MA.newMArray 64 (square White Empty)
       finalCoord <- foldM (accumPlacement (bitbs, sqs)) (Just (0, 7)) s
       case finalCoord of
         Nothing -> return Nothing
         Just _ -> do
-          frozenBitbs <- MA.freezeS bitbs
-          frozenSqs <- MA.freezeS sqs
-          return (Just (frozenBitbs, frozenSqs))
+          return (Just (bitbs, sqs))
 
     accumPlacement :: (MArray s A.P Int Bitboard, MArray s A.P Int Square) -> Maybe (Int, Int) -> Char -> ST s (Maybe (Int, Int))
     accumPlacement _ Nothing _ = return Nothing
@@ -113,11 +222,13 @@ parseFen fen = splitFen (words fen) >>= parseInfo
         ('/', _, _) -> return $ Just (0, y - 1)
         (_, Just dx, _) -> return $ Just (x + dx, y)
         (_, _, Just (color, piece)) -> do
-          MA.modify_ bitbs (return . xor (bit $ posFromCoord (x, y))) (fromEnum piece + 2)
-          MA.modify_ bitbs (return . xor (bit $ posFromCoord (x, y))) (fromEnum color)
+          MA.modify_ bitbs (return . xor (bit $ posFromCoord (x, y))) $ pieceInd piece
+          MA.modify_ bitbs (return . xor (bit $ posFromCoord (x, y))) $ fromEnum color
           MA.write_ sqs (posFromCoord (x, y)) (square color piece)
           return $ Just (x + 1, y)
         _ -> return Nothing
+
+    castleInd c = snd $ head $ filter ((== c) . fst) [('K', 0), ('Q', 1), ('k', 2), ('q', 3)]
 
     charPiece =
       Map.fromList
@@ -126,20 +237,29 @@ parseFen fen = splitFen (words fen) >>= parseInfo
         , (c, p) <- [('P', Pawn), ('R', Rook), ('N', Knight), ('B', Bishop), ('Q', Queen), ('K', King)]
         ]
 
-    adjustCastle c ((wk, wq), (bk, bq)) =
-      case c of
-        'K' -> Just ((True, wq), (bk, bq))
-        'Q' -> Just ((wk, True), (bk, bq))
-        'k' -> Just ((wk, wq), (True, bq))
-        'q' -> Just ((wk, wq), (bk, True))
-        _ -> Nothing
-
     parseEpCol c
       | c >= 'a' && c <= 'h' = Just $ ord c - ord 'a'
       | otherwise = Nothing
 
     splitFen [placement, stm, castle, ep, halfMove, fullMove] = Just (placement, stm, castle, ep, halfMove, fullMove)
     splitFen _ = Nothing
+
+{-# INLINE pieceInd #-}
+pieceInd :: Piece -> Int
+pieceInd = (+ 2) . fromEnum
+
+{-# INLINE fromBool #-}
+fromBool :: Bool -> a -> Maybe a
+fromBool b = if b then Just else const Nothing
+
+{-# INLINE colorDir #-}
+colorDir :: Player -> Int
+colorDir White = 1
+colorDir Black = -1
+
+{-# INLINE flipColor #-}
+flipColor :: Player -> Player
+flipColor p = toEnum $ 1 - fromEnum p
 
 {-# INLINE square #-}
 square :: Player -> Piece -> Square
@@ -153,6 +273,10 @@ squareInfo s = (toEnum $ fromIntegral $ shiftR s 1, toEnum $ fromIntegral $ s .&
 newMove :: Int -> Int -> Piece -> MoveType -> Move
 newMove sq1 sq2 p t = fromIntegral $ sq1 .^. shiftL sq2 6 .^. shiftL (fromEnum p) 12 .^. shiftL (fromEnum t) 14
 
+{-# INLINE moveInfo #-}
+moveInfo :: Move -> (Int, Int, MoveType, Piece)
+moveInfo m = (square1 m, square2 m, moveType m, promotion m)
+
 {-# INLINE square1 #-}
 square1 :: Move -> Int
 square1 m = fromIntegral $ m .&. 0x3f
@@ -161,13 +285,13 @@ square1 m = fromIntegral $ m .&. 0x3f
 square2 :: Move -> Int
 square2 m = fromIntegral $ shiftR (m .&. 0xfc0) 6
 
-{-# INLINE moveType #-}
-moveType :: Move -> MoveType
-moveType m = toEnum $ fromIntegral $ shiftR (m .&. 0x3000) 12
-
 {-# INLINE promotion #-}
 promotion :: Move -> Piece
-promotion m = toEnum $ fromIntegral $ shiftR (m .&. 0xc000 + 1) 14
+promotion m = toEnum $ fromIntegral $ shiftR (m .&. 0x3000 + 1) 12
+
+{-# INLINE moveType #-}
+moveType :: Move -> MoveType
+moveType m = toEnum $ fromIntegral $ shiftR (m .&. 0xc000) 14
 
 showMove :: Move -> String
 showMove m =
@@ -299,6 +423,27 @@ showBitboard bb =
       : map
         (\y -> map (\x -> if not $ testBit bb $ posFromCoord (x, y) then '.' else '#') [0 .. 7])
         [7, 6 .. 0]
+
+showMArr :: (A.Prim a) => (a -> String) -> MArray s A.P Int a -> ST s String
+showMArr f arr = do
+  l <- A.toList <$> MA.freezeS arr
+  return $ unlines $ map f l
+
+showSqs :: MArray s A.P Int Square -> ST s String
+showSqs arr = do
+  l <- A.toList <$> MA.freezeS arr
+  return $ unlines $ chunksOf 8 $ map (squareChar . squareInfo) l
+  where
+    squareChar (p, c) =
+      chr $
+        fromIntegral (fromEnum c) * (ord 'a' - ord 'A') + case p of
+          Rook -> ord 'R'
+          Bishop -> ord 'B'
+          Pawn -> ord 'P'
+          Knight -> ord 'N'
+          King -> ord 'K'
+          Queen -> ord 'Q'
+          _ -> ord '.'
 
 {-# INLINE posFromCoord #-}
 posFromCoord :: (Int, Int) -> Int
